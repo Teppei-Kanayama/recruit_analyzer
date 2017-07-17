5b6a2544528d037ee61f2258cb943b1583118ccc
Teppei Kanayana <1994teppei0615kanayama@gmail.com>
2017-07-18 00:13:49 +0900

    show_univ was added

--- recruit60_analyze.py
+++ recruit60_analyze.py
@@ -19,7 +19,7 @@ join = pd.read_excel(filename, index_col=0, sheetname="join_sheet")
 course =  applyment.ix[:, 7]
 grade = applyment.ix[:, 8]
 bunri = applyment.ix[:, 5]
-univ = applyment.ix[: 4]
+univ = applyment.ix[:, 4]
 
 def show_grade_and_bunri(sheet, title, output_path):
     #文系低学年・文系高学年・理系低学年・理系高学年・医療系低学年・医療系高学年のそれぞれの割合を調べる。
@@ -52,7 +52,31 @@ def show_grade_and_bunri(sheet, title, output_path):
     plt.title(title)
     plt.savefig(output_path, bbox_inches='tight',pad_inches=0.05)
 
+def show_univ(sheet, title, output_path):
+    #各大学ごとの人数を調べる。
+    univ_dict = {}
+    for i in list(sheet.index):
+        if type(univ[i]) is str:
+            try:
+                univ_dict[univ[i]] += 1
+            except KeyError:
+                univ_dict[univ[i]] = 1
+
+    sorted_index = np.argsort(np.array(list(univ_dict.values())))
+
+    sorted_values = []
+    sorted_keys = []
+    for ind in sorted_index:
+        sorted_values.append(list(univ_dict.values())[ind])
+        sorted_keys.append(list(univ_dict.keys())[ind])
+
+    plt.figure(figsize=(15, 10))
+    plt.title(title, fontsize=18)
+    plt.bar(np.array(range(len(sorted_keys))) * 10, sorted_values[::-1], tick_label=sorted_keys[::-1], align="center", width=5)
+    plt.savefig(output_path)
+
 def show_grade(sheet, title, output_path):
+
     grade_list = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0])
 
     for i in list(sheet.index):
@@ -73,12 +97,18 @@ def show_grade(sheet, title, output_path):
 
 def main():
     #show_grade(comment1, "第一回説明会参加者の学年分布", "./figures/test.png")
-    #print(matplotlib.matplotlib_fname())
+    """
     show_grade_and_bunri(applyment, "説明会申込者の文理・学年", "./figures/applyment_bunri-grade.png")
     show_grade_and_bunri(comment1, "第一回説明会参加者の文理・学年", "./figures/comment1_bunri-grade.png")
     show_grade_and_bunri(comment2, "第二回説明会参加者の文理・学年", "./figures/comment2_bunri-grade.png")
     show_grade_and_bunri(comment3, "第三回説明会参加者の文理・学年", "./figures/comment3_bunri-grade.png")
     show_grade_and_bunri(join, "入ゼミ者の文理・学年", "./figures/join_bunri-grade.png")
+    """
+    show_univ(applyment, "説明会申込者の所属大学", "./figures/applyment_univ.png")
+    show_univ(comment1, "第一回説明会参加者の所属大学", "./figures/comment1_univ.png")
+    show_univ(comment2, "第二回説明会参加者の所属大学", "./figures/comment2_univ.png")
+    show_univ(comment3, "第三回説明会参加者の所属大学", "./figures/comment3_univ.png")
+    show_univ(join, "入ゼミ者の所属大学", "./figures/join_univ.png")
 
 if __name__ == "__main__":
     main()
